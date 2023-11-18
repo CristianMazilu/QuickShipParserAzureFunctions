a487dd0f8a0feaf569dec7928a6369e496efe6b2
Cristian Mazilu <mazilu6@gmail.com>
2023-11-18 21:33:19 +0200

    Connected blob storage. Wrote validation logic. Not tested!

--- QuickShipParser/QuickShipParse.cs
+++ QuickShipParser/QuickShipParse.cs
@@ -1,12 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs;
+using System.Collections.Generic;
 
 namespace QuickShipParser
 {
-    internal class QuickShipParse
+    public class QuickShipParse
     {
+        private const string BlobContainerName = "quickship-az-fn-config";
+
+        [FunctionName("QuickShipParse")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string model = req.Query["model"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            model = model ?? data?.model;
+
+            // Create a Blob Service Client
+            string connectionString = Environment.GetEnvironmentVariable("quickship-az-fn-config-token");
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+            // Access the container
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(BlobContainerName);
+
+            // Get all the *.json files content and store it in a list of strings
+            List<string> jsonFileContents = new List<string>();
+            await foreach (BlobItem blobItem in containerClient.GetBlobsAsync())
+            {
+                if (Path.GetExtension(blobItem.Name).Equals(".json", StringComparison.OrdinalIgnoreCase))
+                {
+                    BlobClient blobClient = containerClient.GetBlobClient(blobItem.Name);
+                    BlobDownloadInfo download = await blobClient.DownloadAsync();
+                    using (StreamReader reader = new StreamReader(download.Content))
+                    {
+                        jsonFileContents.Add(await reader.ReadToEndAsync());
+                    }
+                }
+            }
+
+            IMatch result = new FailedMatch("not found");
+            foreach (var jsonContent in jsonFileContents)
+            {
+                var modelStructure = ModelStructure.FromJson(jsonContent);
+                var modelParser = new ModelParser(modelStructure);
+
+                result = modelParser.Match(model);
+            }
+
+            string responseMessage = string.IsNullOrEmpty(model)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Model string {model} is {result.Success()}. Error is in the following part of the string: {result.RemainingText()}";
+
+            return new OkObjectResult(responseMessage);
+        }
     }
 }
---
+++ QuickShipParserTests/ModelParserFacts.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickShipParser.Facts
+{
+    [TestClass()]
+    public class ModelParserFacts
+    {
+        private ModelStructure SetUpModelStructure()
+        {
+            string json = @"
+            {
+                ""modelName"": ""Mag meters"",
+                ""baseModel"": ""8705"",
+                ""elements"": [
+                    {
+                        ""codeName"": ""Base"",
+                        ""length"": 4,
+                        ""codes"": [
+                            { ""code"": ""8705"", ""description"": ""Magnetic Flowmeter Sensor - Flanged"" }
+                        ]
+                    }
+                ]
+            }";
+            return ModelStructure.FromJson(json);
+        }
+
+        [TestMethod]
+        public void Match_WithValidModelString_ReturnsSuccess()
+        {
+            var modelStructure = SetUpModelStructure();
+            var parser = new ModelParser(modelStructure);
+
+            var result = parser.Match("8705"); // Replace with an appropriate valid model string
+            Assert.IsTrue(result.Success());
+        }
+
+        [TestMethod]
+        public void Match_WithInvalidModelString_ReturnsFailure()
+        {
+            var modelStructure = SetUpModelStructure();
+            var parser = new ModelParser(modelStructure);
+
+            var result = parser.Match("InvalidString");
+            Assert.IsFalse(result.Success());
+        }
+
+        [TestMethod]
+        public void Match_WithPartialValidModelString_ReturnsFailure()
+        {
+            var modelStructure = SetUpModelStructure();
+            var parser = new ModelParser(modelStructure);
+
+            var result = parser.Match("8705Partial"); // A partially valid string
+            Assert.IsFalse(result.Success());
+        }
+
+        [TestMethod]
+        public void Match_WithEmptyModelString_ReturnsFailure()
+        {
+            var modelStructure = SetUpModelStructure();
+            var parser = new ModelParser(modelStructure);
+
+            var result = parser.Match("");
+            Assert.IsFalse(result.Success());
+        }
+
+        [TestMethod]
+        public void Match_WithValidModelString_ReturnsCorrectRemainingText()
+        {
+            var modelStructure = SetUpModelStructure();
+            var parser = new ModelParser(modelStructure);
+
+            var result = parser.Match("8705"); // Replace with an appropriate valid model string
+            Assert.AreEqual("", result.RemainingText()); // Assuming the whole string is used
+        }
+    }
+}
